e02ba376d9f720861dbeb225b767e33188702f81
Maciej Antczak <sokzokna@gmail.com>
2022-04-06 23:28:17 +0200

    implemented repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, repetitions) {
+  let repeatedString = [];
+  for (let i = 1; i <= repetitions; i++) {
+    repeatedString.push(string);
+  }
+  repeatedString = repeatedString.join('');
+  return repeatedString;
 };
 
 // Do not edit below this line
