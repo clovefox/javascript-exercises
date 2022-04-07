bd721fc97fb68813e52872c8c1a6a8a64d12d3a9
Maciej Antczak <sokzokna@gmail.com>
2022-04-07 11:27:09 +0200

    Implemented function to reverse the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let stringArr = string.split("");
+  let reverseArr = [];
 
+  for (let i = 0; i < string.length; i++) {
+    reverseArr.unshift(string[i]);
+  }
+
+  let reversedString = reverseArr.join('');
+
+  return reversedString;
 };
 
 // Do not edit below this line
