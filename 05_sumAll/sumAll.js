eee617f3b58b0026cee96290cad7c9c9c3c925ec
Maciej Antczak <sokzokna@gmail.com>
2022-04-08 15:02:42 +0200

    implemented function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,38 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+  let numbers = [];
+  let sum = null;
+  let allNumbers = true;
+
+  if (typeof number1 === 'number' && typeof number2 === 'number') {
+    allNumbers = true;
+  } else {
+    allNumbers = false;
+  }
+
+  if (allNumbers && (number1 >= 0 && number2 >= 0)) {
+
+    if (number1 < number2) {
+      for (let i = number1; i <= number2; i++) {
+        numbers.push(i);
+      }
+
+    } else if (number1 > number2) {
+      for (let i = number2; i <= number1; i++) {
+        numbers.push(i);
+      }
+    }
+
+  } else {
+
+    return 'ERROR';
+
+  }
+
+  for (let value of numbers) {
+    sum += value;
+  }
+
+  return sum;
 
 };
 
