f42cf3d1a0bf058c184d11d4b1af209a4b43e6d5
Maciej Antczak <sokzokna@gmail.com>
2022-04-08 14:10:01 +0200

    implemented the function to remove elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elements) {
 
+  for (let value of elements) {
+    let elementIndex = array.indexOf(value);
+    if (elementIndex !== -1) {
+      array.splice(elementIndex, 1);
+    }
+  }
+
+  return array;
 };
+
+removeFromArray([1, 2, 3, 4, 5], 1, 2, 3, 4);
 
 // Do not edit below this line
 module.exports = removeFromArray;
