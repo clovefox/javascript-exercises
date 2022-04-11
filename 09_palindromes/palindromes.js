47e1ce07d86fc3d7cfa4e0bc9b1514bcc3c25dc6
Maciej Antczak <sokzokna@gmail.com>
2022-04-11 15:55:17 +0200

    implemented the function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+  let stringRemoveSpecialCharacters = (string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")).replace(/\s{2,}/g, " ");
+  let stringModifiedRemoveSpaces = stringRemoveSpecialCharacters.replace(/ /g, "");
+  let stringModifiedToLowerCase = stringModifiedRemoveSpaces.toLowerCase();
+
+
+  let stringToArray = stringModifiedToLowerCase.split("");
+  let reverseArray = [];
+
+  for (let value of stringToArray) {
+    reverseArray.unshift(value);
+  }
+
+  let reverseString = reverseArray.join("");
+
+  return (stringModifiedToLowerCase === reverseString ? true : false);
 };
 
 // Do not edit below this line
