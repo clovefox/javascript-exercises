1ba114003d1a1b59aa3efc138ca47c9d726cc119
Maciej Antczak <sokzokna@gmail.com>
2022-04-11 13:37:14 +0200

    implemented all required functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,56 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arrOfNumbers) {
+  let sum = null;
+
+  if (arrOfNumbers.length === 0) {
+    return 0;
+  }
+
+  for (let value of arrOfNumbers) {
+    sum += value;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (arrOfNumbers) {
+  let result = 1;
 
+  for (let value of arrOfNumbers) {
+    result *= value;
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  let result = 1;
+
+  for (let i = 1; i <= power; i++) {
+    result *= base;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let result = 1;
+
+  if (n === 0) { return 1 }
+
+  for (i = 1; i <= n; i++) {
+    result *= i;
+  }
+
+  return result;
+
 };
 
 // Do not edit below this line
