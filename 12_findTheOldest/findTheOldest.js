ae4f22d24bed242a099f02f64746813b1a710ddd
Maciej Antczak <sokzokna@gmail.com>
2022-04-17 15:55:13 +0200

    implemented the function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  let ageOfPpl = [];
+
+  arr.forEach(element => {
+
+    if ('yearOfDeath' in element === false) {
+      ageOfPpl.push(new Date().getFullYear() - element['yearOfBirth']);
+    } else {
+      ageOfPpl.push(element['yearOfDeath'] - element['yearOfBirth']);
+    }
+
+  })
+
+  let oldest = Math.max(...ageOfPpl);
+  let indexOfOldest = ageOfPpl.indexOf(oldest);
+
+  return arr[indexOfOldest];
 
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
